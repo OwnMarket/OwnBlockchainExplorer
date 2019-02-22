a3ce5ceeabc4f7f9a90d3119d7b1817ca743e46f
Anil Mujagic <anil.mujagic@gmail.com>
2019-02-22 15:17:10 +0100

    Add base class for data interaction services

---
+++ Source/Own.BlockchainExplorer.Domain/Common/DataService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Own.BlockchainExplorer.Core.Enums;
+using Own.BlockchainExplorer.Core.Interfaces;
+
+namespace Own.BlockchainExplorer.Domain.Common
+{
+    public abstract class DataService
+    {
+        private readonly IUnitOfWorkFactory _unitOfWorkFactory;
+        private readonly IRepositoryFactory _repositoryFactory;
+
+        protected DataService(IUnitOfWorkFactory unitOfWorkFactory, IRepositoryFactory repositoryFactory)
+        {
+            _unitOfWorkFactory = unitOfWorkFactory ?? throw new ArgumentNullException(nameof(unitOfWorkFactory));
+            _repositoryFactory = repositoryFactory ?? throw new ArgumentNullException(nameof(repositoryFactory));
+        }
+
+        protected IUnitOfWork NewUnitOfWork(UnitOfWorkMode mode = UnitOfWorkMode.ReadOnly)
+        {
+            return _unitOfWorkFactory.Create(mode);
+        }
+
+        protected IRepository<T> NewRepository<T>(IUnitOfWork unitOfWork)
+            where T : class
+        {
+            return _repositoryFactory.Create<T>(unitOfWork);
+        }
+    }
+}
