a6f82828b559186a3838795163006396113ab66d
Anil Mujagic <anil.mujagic@gmail.com>
2019-02-22 14:19:42 +0100

    Add class for accessing configuration

---
+++ Source/Own.BlockchainExplorer.Core/Config.cs
@@ -0,0 +1,15 @@
+﻿using Own.BlockchainExplorer.Core.Interfaces;
+
+namespace Own.BlockchainExplorer.Core
+{
+    public static class Config
+    {
+        private static IConfigurationProvider _provider;
+        public static void SetConfigurationProvider(IConfigurationProvider provider)
+        {
+            _provider = provider;
+        }
+
+        public static string DB => _provider.GetString("appSettings:DB");
+    }
+}
---
+++ Source/Own.BlockchainExplorer.Core/Interfaces/IConfigurationProvider.cs
@@ -0,0 +1,10 @@
+﻿namespace Own.BlockchainExplorer.Core.Interfaces
+{
+    public interface IConfigurationProvider
+    {
+        string GetString(string settingKey, string defaultValue = null);
+        bool? GetBoolean(string settingKey, bool? defaultValue = null);
+        int? GetInteger(string settingKey, int? defaultValue = null);
+        decimal? GetDecimal(string settingKey, decimal? defaultValue = null);
+    }
+}
