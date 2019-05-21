df13b518efd238b09927f8bef72a0f2b24a0e16e
Ajdin Kahrovic <ajdin.kahrovic@weown.com>
2019-05-21 16:42:00 +0200

    Added logging

---
+++ Source/Own.BlockchainExplorer.Common/Log.cs
@@ -0,0 +1,44 @@
+﻿using Own.BlockchainExplorer.Common.Extensions;
+using Serilog;
+using Serilog.Events;
+using System;
+
+namespace Own.BlockchainExplorer.Common
+{
+    public static class Log
+    {
+        private static ILogger _log;
+
+        public static void Initialize(string fileName)
+        {
+            _log = new LoggerConfiguration().WriteTo.File(fileName).CreateLogger();
+        }
+
+        public static void Debug(string format, params object[] args)
+        {
+#if DEBUG
+            _log.Write(LogEventLevel.Debug, format, args);
+#endif
+        }
+
+        public static void Info(string format, params object[] args)
+        {
+            _log.Write(LogEventLevel.Information, format, args);
+        }
+
+        public static void Warning(string format, params object[] args)
+        {
+            _log.Write(LogEventLevel.Warning, format, args);
+        }
+
+        public static void Error(string format, params object[] args)
+        {
+            _log.Write(LogEventLevel.Error, format, args);
+        }
+
+        public static void Error(Exception ex)
+        {
+            _log.Write(LogEventLevel.Error, ex.LogFormat(), "");
+        }
+    }
+}
