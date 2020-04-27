16460ae10cfb57571e5f987bb463903593573935
Ajdin Kahrovic <ajdin.kahrovic@weown.com>
2020-04-27 19:15:47 +0200

    Changing db script

--- Database/Changes/0014 Add grouping_id to blockchain_event table.sql
+++ Database/Changes/0014 Add grouping_id to blockchain_event table.sql
@@ -6,7 +6,6 @@ DO $$
 BEGIN
 
     ALTER TABLE blockchain_event ADD COLUMN grouping_id uuid;
-    ALTER TABLE blockchain_event ADD CONSTRAINT blockchain_event__uk__grouping_id UNIQUE (grouping_id);
 
     INSERT INTO database_version (version_number, description)
     VALUES (14, 'Add grouping_id to blockchain_event table');
